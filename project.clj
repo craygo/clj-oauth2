fe8a5073840021e93a6a6b0fcfa128417ec49aac
Harry Binnendijk <harry@empanda.net>
2014-03-05 16:13:45 +1100

    up minor version for clojars deploy issue

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
-(defproject craygo/clj-oauth2 "0.1.0"
+(defproject craygo/clj-oauth2 "0.1.1"
   :description "oauth2 basic functions"
+  :url "https://github.com/craygo/clj-oauth2"
+  :scm {:name "git"
+        :url "http://www.eclipse.org/legal/epl-v10.html"}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-http "0.9.0"]])
+                 [clj-http "0.9.0"]]
+  :deploy-repositories [["clojars" {:creds :gpg}]])
